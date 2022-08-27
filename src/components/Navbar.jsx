4098c5dcc3b6979b1944b876912e755a73025ad1
Khushal <Khushalganeshsolanki@gmail.com>
2022-08-27 21:39:39 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
-  const [active, setActive] = useState("Homee");
+  const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
 
   return (
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import { people01, people02, people03, facebook, instagram, linkedin, twitter, a
 export const navLinks = [
   {
     id: "home",
-    title: "Homee",
+    title: "Home",
   },
   {
     id: "features",
