ec65e45d96d709d238722c84b2cc5d2bdbcab7b8
Khushal <Khushalganeshsolanki@gmail.com>
2022-08-27 21:33:13 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
-  const [active, setActive] = useState("Home");
+  const [active, setActive] = useState("Homee");
   const [toggle, setToggle] = useState(false);
 
   return (
